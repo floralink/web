0db3704bc462b0842f6af43e0c36f353170daa7c
schwince <hizzards@mailbox.org>
2022-03-15 07:12:33 +0100

    Bumped Floralink Core to v0.1.2

--- src/main.js
+++ src/main.js
@@ -4,7 +4,8 @@ import router from "./router";
 
 // Floralink server connection
 import axios from "axios";
-axios.defaults.baseURL = "http://floralink.herokuapp.com";
+// axios.defaults.baseURL = "http://localhost:3000/";
+axios.defaults.baseURL = "https://floralink-server.onrender.com";
 
 import TheHeader from "./components/TheHeader.vue";
 import TheFooter from "./components/TheFooter.vue";
