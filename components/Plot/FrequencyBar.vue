ab27789d8b725091c628eb16d83970d6a46fc244
schwince <info@schwince.de>
2024-10-31 04:33:00 +0100

    Refactor the FrequencyBar component

--- components/Plot/FrequencyBar.vue
+++ components/Plot/FrequencyBar.vue
@@ -1,28 +1,20 @@
 <script setup lang="ts">
-const props = defineProps({
-  categories: Array,
-  // "categoriesExtra": {
-  //   type: Array,
-  //   default: () => ["x", "?"]
-  // },
-  frequencies: Object,
-  title: {
-    type: String,
-    default: "Absolute Häufigkeiten der Kategorien",
-  },
-  description: {
-    type: String,
-    default: "",
-  },
-  color: {
-    type: String,
-    default: "#89c15a",
-  },
-  filename: {
-    type: String,
-    default: "plot-frequency",
-  },
-});
+const props = withDefaults(
+  defineProps<{
+    categories: string[];
+    frequencies: { [key: string]: number };
+    title: string;
+    description: string;
+    color: string;
+    filename: string;
+  }>(),
+  {
+    title: "Absolute Häufigkeiten",
+    description: "",
+    color: "#89c15a",
+    filename: "plot-frequency",
+  }
+);
 
 const plotLayout = computed(() => ({
   title: props.title,
@@ -35,25 +27,13 @@ const plotLayout = computed(() => ({
   colorway: [props.color],
 }));
 
-const plotData = computed(() => {
-  let trace = [];
-
-  var barPlotX = [];
-  var barPlotY = [];
-
-  props.categories.forEach((possibleValue) => {
-    barPlotX.push(possibleValue);
-    barPlotY.push(props.frequencies[possibleValue] || 0);
-  });
-
-  trace.push({
-    x: barPlotX,
-    y: barPlotY,
+const plotData = computed(() => [
+  {
+    x: props.categories,
+    y: props.categories.map((key) => props.frequencies[key] || 0),
     type: "bar",
-  });
-
-  return trace;
-});
+  },
+]);
 </script>
 
 <template>
