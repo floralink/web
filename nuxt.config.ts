f3e8ff421c41814d7f777662743d7c934fce196b
schwince <info@schwince.de>
2025-02-12 22:48:29 +0100

    Update site URL

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -21,10 +21,10 @@ export default defineNuxtConfig({
 
   // Meta
   site: {
-    url: "https://www.flora-link.de",
+    url: "www.flora-link.de",
     name: "Floralink",
     description:
-      "Floralink ist ein Online-Tool zur statistischen Auswertung von floristischen Kartierungen. Die Ergebnisse können dann mit taxonspezifischen Daten (wie die Zeigerwerte nach Ellenberg oder der Rote-Liste-Status) verknüpft werden. Daraus werden Lage- und Streuungsmaße berechnet und in Diagrammen visualisiert. ",
+      "Floralink ist ein Online-Tool zur statistischen Auswertung von floristischen Kartierungen. Die Ergebnisse können dann mit taxonspezifischen Daten (wie die Zeigerwerte nach Ellenberg oder der Rote-Liste-Status) verknüpft werden. Daraus werden Lage- und Streuungsmaße berechnet und in Diagrammen visualisiert.",
     defaultLocale: "de",
   },
   app: {
