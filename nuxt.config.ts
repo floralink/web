590571d732e32428263b20cf6729b627eba084fa
schwince <info@schwince.de>
2025-06-25 01:20:58 +0200

    style(nuxt.config.ts): order config keys alphabetically and format with prettier

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,26 +1,4 @@
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: "2024-10-12",
-  css: ["~/assets/css/main.css"],
-  devtools: { enabled: true },
-  modules: ["@pinia/nuxt", "@nuxt/ui", "@nuxt/fonts", "@vueuse/nuxt", "@nuxtjs/seo", "nuxt-security"],
-  runtimeConfig: {
-    public: {
-      imprintName: "",
-      imprintAddress: "",
-      imprintEmail: "",
-    },
-  },
-  ssr: false,
-
-  // Meta
-  site: {
-    url: "www.flora-link.de",
-    name: "Floralink",
-    description:
-      "Floralink ist ein Online-Tool zur statistischen Auswertung von floristischen Kartierungen. Die Ergebnisse können dann mit taxonspezifischen Daten (wie die Zeigerwerte nach Ellenberg oder der Rote-Liste-Status) verknüpft werden. Daraus werden Lage- und Streuungsmaße berechnet und in Diagrammen visualisiert.",
-    defaultLocale: "de",
-  },
   app: {
     head: {
       link: [
@@ -35,15 +13,39 @@ export default defineNuxtConfig({
       },
     },
   },
-
-  // SEO
-  seo: {
-    fallbackTitle: false,
-    redirectToCanonicalSiteUrl: true,
-  },
+  compatibilityDate: "2024-10-12",
+  css: ["~/assets/css/main.css"],
+  devtools: { enabled: true },
+  modules: [
+    "@pinia/nuxt",
+    "@nuxt/ui",
+    "@nuxt/fonts",
+    "@vueuse/nuxt",
+    "@nuxtjs/seo",
+    "nuxt-security",
+  ],
+  ogImage: { enabled: false },
   routeRules: {
     "/report/**": { robots: false },
     "/floralink/imprint": { robots: false },
   },
-  ogImage: { enabled: false },
+  runtimeConfig: {
+    public: {
+      imprintName: "",
+      imprintAddress: "",
+      imprintEmail: "",
+    },
+  },
+  seo: {
+    fallbackTitle: false,
+    redirectToCanonicalSiteUrl: true,
+  },
+  site: {
+    url: "www.flora-link.de",
+    name: "Floralink",
+    description:
+      "Floralink ist ein Online-Tool zur statistischen Auswertung von floristischen Kartierungen. Die Ergebnisse können dann mit taxonspezifischen Daten (wie die Zeigerwerte nach Ellenberg oder der Rote-Liste-Status) verknüpft werden. Daraus werden Lage- und Streuungsmaße berechnet und in Diagrammen visualisiert.",
+    defaultLocale: "de",
+  },
+  ssr: false,
 });
